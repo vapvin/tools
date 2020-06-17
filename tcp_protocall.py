0c3d45f5a77bcd059bd0ae79a7b2f8b831221d9a
vins <vins@kali-linux.localdomain>
2020-06-17 14:08:57 +0900

    Adding Network Tools

---
+++ tcp_protocall.py
@@ -0,0 +1,34 @@
+import socket
+import threading
+
+ip = "0.0.0.0"
+port = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((ip,port))
+
+server.listen(5)
+
+print '[*] Listening on %s:%d' % (ip, port)
+
+def handle_client(client_socket):
+
+    request = client_socket.recv(1024)
+
+    print '[*] Received: %s' % request
+
+    client_socket.send("ACK!")
+
+    client_socket.close()
+
+while True:
+
+    client, addr = server.accept()
+
+    print '[*] Accepted connection from %s:%d' % (addr[0], addr[1])
+
+    client_handler = threading.Thread(target=handle_client, args=(client,))
+    client_handler.start()
+
+            
