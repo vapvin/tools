0f86c7c585b5373db0f53c2cb3561acad759f6ef
vapvin <tpehdj12@daum.net>
2020-06-20 19:27:06 +0900

    What's the meta?

--- netcat.py
+++ netcat.py
@@ -18,3 +18,15 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((ip,port))
 
 server.listen(5)
+
+print '[*] Listening on %s:%d' % (ip, port)
+
+def handle_client(client_socket):
+
+    request = client_socket.recv(1024)
+
+    print '[*] Received: %s' % request
+
+    client_socket.send("ACK!")
+
+    client_socket.close()
